57fe3d1d18217acf30102df34e9778f50b08fa22
Martin <fmartinhuber@gmail.com>
2015-05-13 21:59:20 -0300

    Importar

---
+++ tp-distribuidas-cliente/src/test.java
@@ -0,0 +1,55 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import dto.ClienteDto;
+import dto.HabilitadoDto;
+import rmi.IAdministradorVarios;
+import rmi.IAdministradorViaje;
+
+
+public class test {
+
+
+	private IAdministradorVarios administradorV;
+	
+	public test() {
+		this.administradorV = obtenerStub();
+	}
+	
+	//Obtener STUB
+	private IAdministradorVarios obtenerStub(){
+		try {
+			//Para ejecutarlo, primero hay que ejecutar el servidor. 
+			//Busca el servicio con en el nombre calcular. 
+			return (IAdministradorVarios) (Naming.lookup("AdministradorVarios"));
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Probando commit con otra forma de importar el proyecto
+		System.out.println("Prueba RMI");
+		ClienteDto cliente = new ClienteDto("dni", 34000000);
+		IAdministradorVarios admin = new test().obtenerStub();
+		try {
+			admin.altaCliente(cliente);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
